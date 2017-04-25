b18aa859bd174ccffe71463b2695cd8a10af2040
sashapotash29 <sashapotash29@gmail.com>
2017-04-25 17:53:52 -0400

    added styling and beginning of JS

--- static/js/home.js
+++ static/js/home.js
@@ -1,1 +1,46 @@
 console.log("JS connected")
+
+var check_sub_valid = function(){
+	const stockChoice = document.getElementById('tickerInput').value
+	const strikeInput = document.getElementById('strikeInput').value
+	const maturityInput = document.getElementById('maturityInput').value
+	const rfRateInput = document.getElementById('rfRateInput').value
+	console.log(stockChoice, strikeInput, maturityInput, rfRateInput)
+	var checking_array = [stockChoice,strikeInput,maturityInput,rfRateInput]
+	for(var i =0; i < checking_array.length; i++){
+		if (checking_array[i] == ""){
+			console.log("false")
+			return false
+
+		}
+		else{
+			continue
+		}
+
+	};
+	return true
+
+
+};
+
+var priceButton = document.getElementById('priceButton')
+
+priceButton.addEventListener("click", function(e){
+	e.preventDefault()
+	var answer = check_sub_valid()
+
+	if(answer==true){
+		console.log("ready to go")
+		const stockChoice = document.getElementById('tickerInput').value
+		const strikeInput = document.getElementById('strikeInput').value
+		const maturityInput = document.getElementById('maturityInput').value
+		const rfRateInput = document.getElementById('rfRateInput').value
+		var information_obj = {"stockChoice": stockChoice, "strikeInput":strikeInput,
+			'maturityInput': maturityInput, 'rfRateInput':rfRateInput}
+	}
+	else{
+		console.log("NOT ready to go")
+	}
+
+
+})
