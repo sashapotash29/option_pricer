2c77712bc7e9f0342bb04d3f52be9135ac0dcca5
sashapotash29 <sashapotash29@gmail.com>
2017-05-01 12:54:26 -0400

    sending info to server

--- tools.py
+++ tools.py
@@ -1,3 +1,14 @@
 
 def price_option(variable_object):
+	strike_price = variable_object['strikeInput']
+	maturity = variable_object['maturityInput']
+	rf_rate = variable_object['rfRateInput']
+
+
+
+def d1_calc(argument):
+	pass
+
+
+def d2_calc(argument):
 	pass
