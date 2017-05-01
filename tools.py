b5d913d4d6045d0ef45bd81681dac873762d2728
sashapotash29 <sashapotash29@gmail.com>
2017-05-01 17:30:49 -0400

    grab stock data into DF

--- tools.py
+++ tools.py
@@ -1,3 +1,21 @@
+import requests
+from datetime import datetime, date
+import pandas as pd
+import io
+
+
+
+def grab_stock_csv(ticker):
+	today = date.today()
+	day = str(today.day)
+	year = str(today.year)
+	month = str(today.month)
+	print(month, day, year)
+	url = 'http://chart.finance.yahoo.com/table.csv?s='+ticker+'&a='+month+'&b='+day+'&c=2016&d='+month+'&e='+day+'&f='+year+'&g=d&ignore=.csv'
+	result = requests.get(url).content
+	df = pd.read_csv(io.StringIO(result.decode('utf-8')))
+	print(df)
+
 
 def price_option(variable_object):
 	strike_price = variable_object['strikeInput']
@@ -12,3 +30,9 @@ def d1_calc(argument):
 
 def d2_calc(argument):
 	pass
+
+
+
+# testing
+
+grab_stock_csv("aapl")
